1ad2d22bffc58c7650c0e9a08324b6a6f3c8e867
Kaustav Ghosh <teetangh@gmail.com>
2020-01-22 11:36:54 +0530

    DBMS week 3 a lot of work pending

--- DBMS LAB/WEEK 2/q2.sql
+++ DBMS LAB/WEEK 2/q2.sql
@@ -1,17 +1,31 @@
 @C:\Users\student.OSLAB-1031\Desktop\DBSLab\UniversityDB\DDL+drop.sql
 @C:\Users\student.OSLAB-1031\Desktop\DBSLab\UniversityDB\smallRelationsInsertFile.sql
 
-select * from student;
+select *
+from student;
 
-select * from instructor where dept_name='Comp. Sci.';
+select *
+from instructor
+where dept_name='Comp. Sci.';
 
-select * from course where credits = '3';
+select *
+from course
+where credits = '3';
 
-select T.course_id , C.title from takes T,course C where T.course_id = C.course_id and id='12345';
+select T.course_id , C.title
+from takes T, course C
+where T.course_id = C.course_id and id='12345';
 
-select * from instructor where salary between 40000 and 90000
+select *
+from instructor
+where salary between 40000 and 90000
 
-select instructor.id from instructor where instructor.ID not in (select teaches.id from teaches);
+select instructor.id
+from instructor
+where instructor.ID not in (select teaches.id
+from teaches);
 
 
-select student.name,course.title,takes.year from student,course,takes,course,Section where student.id = takes.id and takes.year = Section.year and course.course_id = takes.course_id and section.roomnumber='303';
+select student.name, course.title, takes.year
+from student, course, takes, course, Section
+where student.id = takes.id and takes.year = Section.year and course.course_id = takes.course_id and section.roomnumber='303';
--- DBMS LAB/WEEK 3/slides.sql
+++ DBMS LAB/WEEK 3/slides.sql
@@ -10,3 +10,14 @@ from instructor,
   department
 where
   instructor.dept_name = 'physics';
+
+
+-- 
+
+SELECT name, course_id
+FROM instructor, teaches
+WHERE instructor.ID = teaches.ID and instructor.dept_name = 'Art';
+
+SELECT DISTINCT T.name
+FROM instructor as T, instructor as S
+WHERE T.salary > S.salary and S.dept_name ='Comp. Sci.';
