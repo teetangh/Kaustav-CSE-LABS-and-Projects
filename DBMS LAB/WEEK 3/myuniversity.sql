e2cad41e6b3e6439956cea924206ace66a7ecc36
Kaustav Ghosh <teetangh@gmail.com>
2020-01-26 17:21:08 +0530

    little better at DBMS LAB week 3

--- DBMS LAB/WEEK 3/myuniversity.sql
+++ DBMS LAB/WEEK 3/myuniversity.sql
@@ -15,8 +15,58 @@ MINUS
 select course_id from section where semester = 'Spring' and year = 2010;
 
 -- q4
+-- select course_id,count(*)
+-- from takes
+-- group by course_id;
 
 
 -- q5
-select course_id 
-from section as Spring 
+select distinct course_id
+from section
+where semester = 'Fall' and year = 2009 and 
+course_id in 
+(select course_id
+from section 
+where semester = 'Spring' and year = 2010);
+
+-- q6
+select count(distinct ID)
+from takes
+where (course_id,sec_id,semester,year)
+in
+(select course_id,sec_id,semester,year
+from teaches 
+where teaches.ID = 10101);
+
+-- q7
+select distinct course_id
+from section
+where semester = 'Fall' and year = 2009 and 
+course_id not in 
+(select course_id
+from section 
+where semester = 'Spring' and year = 2010);
+
+-- q8
+select S.name
+from student S
+where S.name in
+(select I.name
+from instructor I);
+-- Better
+select * from instructor,student where instructor.name = student.name;
+
+-- q9
+select *   -- can be only name
+from instructor A
+where A.salary > some
+(select salary
+from instructor B
+where B.dept_name = 'Biology');
+-- Better
+select *    -- can be only name
+from instructor A
+where A.salary >
+(select min(salary)
+from instructor B
+where B.dept_name = 'Biology');
