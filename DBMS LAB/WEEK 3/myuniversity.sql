2734820141f3f6067606d3df67630ea697046960
Kaustav Ghosh <teetangh@gmail.com>
2020-01-26 17:46:49 +0530

    Quite better at DBMS lab week 3

--- DBMS LAB/WEEK 3/myuniversity.sql
+++ DBMS LAB/WEEK 3/myuniversity.sql
@@ -70,3 +70,55 @@ where A.salary >
 (select min(salary)
 from instructor B
 where B.dept_name = 'Biology');
+
+
+-- q10
+select *   -- can be only name
+from instructor A
+where A.salary > all
+(select salary
+from instructor B
+where B.dept_name = 'Biology');
+
+-- q11
+
+-- select *
+-- from instructor A
+-- where (A.dept_name,A.salary) in
+-- (select B.dept_name,avg(B.salary)
+-- from instructor B
+-- group by B.dept_name);
+
+select A.dept_name,avg(A.salary)
+from instructor A
+group by A.dept_name
+having avg(A.salary) >= all
+(select avg(B.salary)
+from instructor B
+group by B.dept_name);
+
+-- -- Brute Force Check
+-- select dept_name , avg(salary)
+-- from instructor
+-- group by dept_name;
+
+
+-- q12
+
+select dept_name
+from department
+where budget <
+(select avg(salary)
+from instructor);
+
+
+-- q13
+select course_id
+from section S
+where S.semester = 'Fall' and year = 2009 and
+exists
+(select course_id
+from section T
+where T.semester = 'Spring' and T.year = 2010);
+
+-- q14
