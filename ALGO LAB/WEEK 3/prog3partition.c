245660690268154e79502bb5cff11598f8e18c77
Kaustav Ghosh <teetangh@gmail.com>
2020-01-27 16:38:56 +0530

    Algo Lab 4 working on it

--- ALGO LAB/WEEK 3/prog3partition.c
+++ ALGO LAB/WEEK 3/prog3partition.c
@@ -1,17 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <stdio.h> 
+#include <stdbool.h> 
 
-int main()
-{
-	int n;
-	scanf(" %d", &n);
+bool isSubsetSum (int arr[], int n, int sum) 
+{ 
 
-	int *arr = (int *)calloc(n, sizeof(int));
+if (sum == 0) 
+	return true; 
+if (n == 0 && sum != 0) 
+	return false; 
+ 
+if (arr[n-1] > sum) 
+	return isSubsetSum (arr, n-1, sum); 
 
-	for (int i = 0; i < n; ++i)
-		scanf(" %d", &arr[i]);
 
-	int *set1 = (int *)calloc(n, sizeof(int));
-	int *set2 = (int *)calloc(n, sizeof(int));
-}
+return isSubsetSum (arr, n-1, sum) || 
+		isSubsetSum (arr, n-1, sum-arr[n-1]); 
+} 
+
+bool findPartiion (int arr[], int n) 
+{ 
+	int sum = 0; 
+	for (int i = 0; i < n; i++) 
+	sum += arr[i]; 
+
+	if (sum%2 != 0) 
+	return false; 
+
+	return isSubsetSum (arr, n, sum/2); 
+} 
+
+int main() 
+{ 
+int arr[] = {3, 1, 5, 9, 12}; 
+int n = sizeof(arr)/sizeof(arr[0]); 
+if (findPartiion(arr, n) == true) 
+	printf("Can be divided into two subsets "
+			"of equal sum"); 
+else
+	printf("Can not be divided into two subsets"
+			" of equal sum"); 
+return 0; 
+} 
