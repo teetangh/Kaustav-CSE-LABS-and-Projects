a414c3a24d94e468cefd102801b54bea623c5ac7
Kaustav Ghosh <teetangh@gmail.com>
2020-02-11 17:13:15 +0530

    ES LAB WEEK 6 HARDWARE FUN

---
+++ ES LAB/WEEK 6/sample.d
@@ -0,0 +1,7 @@
+.\sample.o: sample.c
+.\sample.o: C:\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.h
+.\sample.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+.\sample.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\sample.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\sample.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\sample.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
