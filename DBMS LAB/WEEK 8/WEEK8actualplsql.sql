f53572010fd974c8bb7b1c676e15f85d52fa582a
Kaustav Ghosh <teetangh@gmail.com>
2020-02-19 08:27:22 +0530

    DBMS WEEK 8 lol

--- DBMS LAB/WEEK 8/WEEK8actualplsql.sql
+++ DBMS LAB/WEEK 8/WEEK8actualplsql.sql
@@ -10,13 +10,35 @@ insert into StudentTable values(3,3.4);
 insert into StudentTable values(4,7.8);
 insert into StudentTable values(5,9.5);
 
+select * from StudentTable;
+
+
 SET SERVEROUTPUT ON;
+-- q1
+
+-- DECLARE
+--     sgpa StudentTable.GPA%TYPE;
+--     srollno StudentTable.RollNo%TYPE;
+--     x NUMBER := 0;
+-- BEGIN
+--     -- while x < count(StudentTable.ID)
+--     while x < 5
+--         LOOP
+--             DBMS_OUTPUT.PUT_LINE('Student Roll Number: ' || srollno(x) || 'Student GPA: ' || sgpa );
+--         END LOOP;
+-- END;
+-- /
 
 DECLARE
     sgpa StudentTable.GPA%TYPE;
     srollno StudentTable.RollNo%TYPE;
-    x NUMBER := 0;
 BEGIN
-    while x < count(StudentTable.ID)
-        LOOP
-            DBMS_OUTPUT.PUT_LINE(srollno(x) || sgpa );
+    srollno := '&R';
+    select GPA into sgpa from StudentTable where RollNo = srollno;
+    DBMS_OUTPUT.PUT_LINE('GPA for given student is: ' || TO_CHAR(sgpa));
+END;
+/
+
+-- q2
+
+    
