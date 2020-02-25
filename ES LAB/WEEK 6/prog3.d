8498b1af93c850c49cd41c9d53ffbeec7c119c68
Rahat Santosh <rahatsantosh@gmail.com>
2020-02-25 15:05:12 +0530

    ES LAB WEEK 6 seems okay

---
+++ ES LAB/WEEK 6/prog3.d
@@ -0,0 +1,7 @@
+.\prog3.o: prog3.c
+.\prog3.o: C:\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.h
+.\prog3.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+.\prog3.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\prog3.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\prog3.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\prog3.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
