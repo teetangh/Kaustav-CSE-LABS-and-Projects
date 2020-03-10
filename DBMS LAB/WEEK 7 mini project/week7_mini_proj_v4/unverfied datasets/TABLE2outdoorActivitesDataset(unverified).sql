12aed291e4d6615332582d79f17540f46d198b99
Kaustav Ghosh <teetangh@gmail.com>
2020-03-11 01:19:52 +0530

    added dbms week 7 mini_project v4

---
+++ DBMS LAB/WEEK 7 mini project/week7_mini_proj_v4/unverfied datasets/TABLE2outdoorActivitesDataset(unverified).sql
@@ -0,0 +1,10 @@
+INSERT INTO outdoor_activities VALUES('1000','VIT Vellore','02-Jun-17',7000);
+INSERT INTO outdoor_activities VALUES('1001','SRM Chennai','02-Jun-18',8000);
+INSERT INTO outdoor_activities VALUES('1002','MUJ Jaipur','02-Jun-19',9000);
+INSERT INTO outdoor_activities VALUES('1003','MIT Mangalore','03-May-17',7500);
+INSERT INTO outdoor_activities VALUES('1004','KMC Manipal','01-Feb-17',7654);
+
+
+
+
+SELECT * from outdoor_activities;
---
+++ DBMS LAB/WEEK 7 mini project/week7_mini_proj_v4/verified datasets/TABLE6EquipmentsDataset(verified).sql
@@ -0,0 +1,5 @@
+INSERT INTO Equipments VALUES ('football',60);
+INSERT INTO Equipments VALUES ('basketball',69);
+INSERT INTO Equipments VALUES ('spikes',78);
+INSERT INTO Equipments VALUES ('hurdles',67);
+INSERT INTO Equipments VALUES ('javelin',79);
