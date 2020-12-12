b78a015d06e01cb092ebc1310ca99914fda0f90a
Kaustav Ghosh <teetangh@gmail.com>
2020-12-12 18:54:16 +0530

    updated CD LAB 01 programs

---
+++ CD LAB/LAB 01/lab01_q2new.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    FILE *fa, *fb;
+
+    fa = fopen("input.txt", "r");
+    fb = fopen("output.txt", "w+");
+
+    
+    while ()
+
+        fclose(fa);
+    fclose(fb);
+
+    return 0;
+}
