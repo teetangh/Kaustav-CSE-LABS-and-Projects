12a1b2e235a662200384a5bc7530c7c632cd9852
Kaustav Ghosh <teetangh@gmail.com>
2020-12-15 17:36:03 +0530

    updated OS LAB + removed object files

--- OS LAB/LAB 02/lab02_q1.c
+++ OS LAB/LAB 02/lab02_q1.c
@@ -6,7 +6,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-void ls_l(char *dir)
+void exec_ls_l(char *dir, int depth)
 {
     DIR *dp;
     struct dirent *entry;
@@ -14,44 +14,45 @@ void ls_l(char *dir)
 
     if ((dp = opendir(dir)) == NULL)
     {
-        fprintf(stderr, "Cannot open directory %s \n", dir);
+        fprintf(stderr, "Cannot open Directory %s \n", dir);
         return;
     }
     chdir(dir);
     while ((entry = readdir(dp)) != NULL)
     {
-        stat(entry->d_name, &statbuf);
+        lstat(entry->d_name, &statbuf);
+
         if (S_ISDIR(statbuf.st_mode))
         {
             if ((strcmp(".", entry->d_name) != 0) && (strcmp("..", entry->d_name) != 0))
-                printf(" %*s %s/ \t\t", 1, "", entry->d_name);
+                printf(" %*s %s/ \t\t\t", 1, "", entry->d_name);
         }
         else
-        {
-            printf(" %*s %s \t\t\t", 1, "", entry->d_name);
+            printf("%*s  %s \t\t\t\t", depth, "", entry->d_name);
 
-            printf(S_ISDIR(statbuf.st_mode) ? "d" : "_");
+        printf(S_ISDIR(statbuf.st_mode) ? "d" : "_");
+        printf((statbuf.st_mode & S_IRUSR) ? "r" : "-");
+        printf((statbuf.st_mode & S_IWUSR) ? "w" : "-");
+        printf((statbuf.st_mode & S_IXUSR) ? "x" : "-");
 
-            printf(statbuf.st_mode & S_IRUSR ? "r" : "-");
-            printf(statbuf.st_mode & S_IWUSR ? "w" : "-");
-            printf(statbuf.st_mode & S_IXUSR ? "x" : "-");
+        printf((statbuf.st_mode & S_IRGRP) ? "r" : "-");
+        printf((statbuf.st_mode & S_IWGRP) ? "w" : "-");
+        printf((statbuf.st_mode & S_IXGRP) ? "x" : "-");
 
-            printf(statbuf.st_mode & S_IRGRP ? "r" : "-");
-            printf(statbuf.st_mode & S_IWGRP ? "w" : "-");
-            printf(statbuf.st_mode & S_IXGRP ? "x" : "-");
+        printf((statbuf.st_mode & S_IROTH) ? "r" : "-");
+        printf((statbuf.st_mode & S_IWOTH) ? "w" : "-");
+        printf((statbuf.st_mode & S_IXOTH) ? "x" : "-");
 
-            printf(statbuf.st_mode & S_IROTH ? "r" : "-");
-            printf(statbuf.st_mode & S_IWOTH ? "w" : "-");
-            printf(statbuf.st_mode & S_IXOTH ? "x" : "-");
-        }
         printf("\n");
     }
+
     chdir("..");
     closedir(dp);
 }
 
 int main(int argc, char const *argv[])
 {
-    ls_l("/home/student/Desktop/KaustavLABS3/OS LAB/LAB 02");
+
+    exec_ls_l("/home/kaustav/Desktop/KaustavLABS3/OS LAB/LAB 02", 1);
     return 0;
 }
