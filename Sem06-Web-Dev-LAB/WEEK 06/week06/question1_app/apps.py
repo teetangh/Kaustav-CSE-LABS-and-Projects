4f890dad1ea69b7674f6625779eb8e94effc2dba
Kaustav Ghosh <teetangh@gmail.com>
2021-05-24 11:28:03 +0530

    it lab week 06: added question{1..4} django apps

---
+++ Sem06-Web-Dev-LAB/WEEK 06/week06/question1_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Question1AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'question1_app'
---
+++ Sem06-Web-Dev-LAB/WEEK 06/week06/week06/urls.py
@@ -0,0 +1,25 @@
+"""week06 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("question1_app", include("question1_app.urls")),
+    # path("question2_app", include("question2_app.urls")),
+    # path("question3_app", include("question3_app.urls")),
+    # path("question4_app", include("question4_app.urls"))
+]
