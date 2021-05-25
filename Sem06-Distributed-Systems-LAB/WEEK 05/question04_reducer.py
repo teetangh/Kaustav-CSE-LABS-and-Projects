2db4851106380703380dbb7dbdc73ce2b1f7e869
Kaustav Ghosh <teetangh@gmail.com>
2021-05-25 18:16:14 +0530

    working DS LAB 05 question4

---
+++ Sem06-Distributed-Systems-LAB/WEEK 05/question04_reducer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import sys
+from operator import itemgetter
+from itertools import groupby
+"""A more advanced Reducer, using Python iterators and generators."""
+
+
+def read_mapper_output(file, separator):
+    # print(separator)
+    for line in file:
+        # print(line.rstrip().split(separator, 100))
+        yield (line.rstrip().split(separator, 100))
+
+
+def main(separator):
+
+    # input comes from STDIN (standard input)
+    data = read_mapper_output(sys.stdin, separator=separator)
+    # groupby groups multiple word-count pairs by word,
+    # and creates an iterator that returns consecutive keys and their group:
+    # current_word - string containing a word (the key)
+    # group - iterator yielding all ["&lt;current_word&gt;", "&lt;count&gt;"] items
+
+    for current_word, group in groupby(data, itemgetter(0)):
+        try:
+            total_count = sum(int(count) for current_word, count in group)
+            print("%s%s%d" % (current_word, separator, total_count))
+        except ValueError:
+            # count was not a number, so silently discard this item
+            pass
+
+
+if __name__ == "__main__":
+    main("\t")
