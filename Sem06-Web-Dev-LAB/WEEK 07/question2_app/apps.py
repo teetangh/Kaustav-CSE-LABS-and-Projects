6707df58ac0aa83a47535de880f46e69efe1301d
Kaustav Ghosh <teetangh@gmail.com>
2021-05-27 13:45:40 +0530

    working IT LAB 07

---
+++ Sem06-Web-Dev-LAB/WEEK 07/question2_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Question2AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'question2_app'
---
+++ Sem06-Web-Dev-LAB/WEEK 07/question4_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Question4AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'question4_app'
